a5819a1bfa0574a24bb5ea6b5f9c11680fd73ead
Stefan Freitag <stefan.freitag@udo.edu>
2020-01-03 13:41:01 +0100

    Set default screen to Sensor view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import {MeasurementsComponent} from './measurements/measurements.component';
 
 export const appRoutes: Routes = [
   { path: 'sensors', component: SensorsComponent },
-  { path: 'measurements',      component: MeasurementsComponent }
-
-  // { path: '**', component: PageNotFoundComponent }
+  { path: 'measurements',      component: MeasurementsComponent },
+  { path: '', component: SensorsComponent },
 ];
